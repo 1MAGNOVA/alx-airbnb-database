3e379e5a4c301d03c79c1838491c0ac43eb7897f
1magnova <olalekanabsm@gmail.com>
2025-10-21 09:27:07 -0700

    script

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -6,3 +6,35 @@ FROM bookings
 INNER JOIN users
   ON bookings.user_id = users.id;
 
+
+
+
+
+SELECT
+  properties.name AS property_name,
+  reviews.rating
+FROM properties
+LEFT JOIN reviews
+  ON properties.id = reviews.property_id;
+
+
+
+
+SELECT
+  users.name AS user_name,
+  bookings.id AS booking_id,
+  bookings.property_id
+FROM users
+LEFT JOIN bookings
+  ON users.id = bookings.user_id
+
+UNION
+
+SELECT
+  users.name AS user_name,
+  bookings.id AS booking_id,
+  bookings.property_id
+FROM bookings
+RIGHT JOIN users
+  ON users.id = bookings.user_id;
+
